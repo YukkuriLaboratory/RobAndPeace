62184db1fb776859e47b85e752dd2fdcf7d54978
turtton <top.gear7509@turtton.net>
2024-11-16 23:13:06 +0900

    test: Add mob attack test

--- src/gametest/kotlin/test/player/TestPlayerAttack.kt
+++ src/gametest/kotlin/test/player/TestPlayerAttack.kt
@@ -1,10 +1,14 @@
 package test.player
 
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest
 import net.minecraft.entity.EntityType
 import net.minecraft.test.GameTest
 import net.minecraft.test.TestContext
 import net.minecraft.world.GameMode
+import net.yukulab.robandpeace.DelegatedLogger
+import util.addInstantFinalTaskWithAssertion
 
 class TestPlayerAttack : FabricGameTest {
     @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
@@ -17,5 +21,21 @@ class TestPlayerAttack : FabricGameTest {
             context.assertEquals(target.health, currentHealth, "Target health changed")
             context.assertEquals(target.primeAdversary, player, "Target prime adversary changed")
         }
+    }
+
+    @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+    fun shouldMobAttackCorrectry(context: TestContext) {
+        val player = context.createMockPlayer(GameMode.SURVIVAL)
+        val target = context.spawnMob(EntityType.HUSK, 0, 0, 0)
+        val currentHealth = player.health
+        context.addInstantFinalTaskWithAssertion(logger) {
+            target.tryAttack(player)
+            player.health shouldNotBe currentHealth
+            player.primeAdversary shouldBe target
+        }
+    }
+
+    companion object {
+        private val logger by DelegatedLogger()
     }
 }
