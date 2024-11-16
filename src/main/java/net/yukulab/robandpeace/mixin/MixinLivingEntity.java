389a0c628dbf82ef0df8affaa7dcdb758cc8ce99
turtton <top.gear7509@turtton.net>
2024-11-16 14:35:11 +0900

    feat: Replace player attack

---
+++ src/main/java/net/yukulab/robandpeace/mixin/MixinLivingEntity.java
@@ -0,0 +1,106 @@
+package net.yukulab.robandpeace.mixin;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import net.yukulab.robandpeace.config.RapConfigs;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class MixinLivingEntity {
+    @Unique
+    long robandpeace$stealCooldown = 0;
+
+    @Shadow
+    protected abstract void dropLoot(DamageSource damageSource, boolean causedByPlayer);
+
+    @Shadow
+    protected abstract void dropXp(@Nullable Entity attacker);
+
+    @Shadow
+    protected int playerHitTimer;
+
+    @Inject(
+            method = "damage",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void checkStealCooldown(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (source.getAttacker() instanceof PlayerEntity) {
+            if (robandpeace$stealCooldown > 0) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+
+    @WrapWithCondition(
+            method = "damage",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/World;sendEntityDamage(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;)V"
+            )
+    )
+    private boolean disableDamageSendingIfPlayerAttacked(World instance, Entity entity, DamageSource damageSource) {
+        return !(damageSource.getAttacker() instanceof PlayerEntity);
+    }
+
+    @WrapWithCondition(
+            method = "damage",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/LivingEntity;takeKnockback(DDD)V"
+            )
+    )
+    private boolean disableKnockbackIfPlayerAttacked(LivingEntity instance, double strength, double x, double z, DamageSource damageSource) {
+        return !(damageSource.getAttacker() instanceof PlayerEntity);
+    }
+
+    @WrapWithCondition(
+            method = "damage",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/LivingEntity;playHurtSound(Lnet/minecraft/entity/damage/DamageSource;)V"
+            )
+    )
+    private boolean disableHurtSoundIfPlayerAttacked(LivingEntity instance, DamageSource damageSource) {
+        return !(damageSource.getAttacker() instanceof PlayerEntity);
+    }
+
+    @Inject(
+            method = "applyDamage",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/LivingEntity;applyArmorToDamage(Lnet/minecraft/entity/damage/DamageSource;F)F"
+            ),
+            cancellable = true
+    )
+    private void replacePlayerAttackBehavior(DamageSource source, float amount, CallbackInfo ci) {
+        if (source.getAttacker() instanceof PlayerEntity player) {
+            dropLoot(source, true);
+            playerHitTimer = 100;
+            dropXp(player);
+            robandpeace$stealCooldown = RapConfigs.getServerConfig().stealCoolTimeTick;
+            ci.cancel();
+        }
+    }
+
+    @Inject(
+            method = "tick",
+            at = @At("HEAD")
+    )
+    private void decreaseCooldown(CallbackInfo ci) {
+        if (robandpeace$stealCooldown > 0) {
+            robandpeace$stealCooldown--;
+        }
+    }
+}
