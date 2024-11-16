4edccacfde2fe8f35baa7f11eb26f980aa864619
turtton <top.gear7509@turtton.net>
2024-11-16 22:43:29 +0900

    test: Add player attack test

--- src/gametest/kotlin/test/player/TestPlayerAttack.kt
+++ src/gametest/kotlin/test/player/TestPlayerAttack.kt
@@ -1,13 +1,21 @@
 package test.player
 
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest
+import net.minecraft.entity.EntityType
 import net.minecraft.test.GameTest
 import net.minecraft.test.TestContext
+import net.minecraft.world.GameMode
 
 class TestPlayerAttack : FabricGameTest {
     @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
-    fun testPlayerAttack(context: TestContext) {
-        context.addInstantFinalTask {
+    fun checkPlayerAttackDisabled(context: TestContext) {
+        val player = context.createMockPlayer(GameMode.SURVIVAL)
+        val target = context.spawnMob(EntityType.PIG, 0, 0, 0)
+        val currentHealth = target.health
+        context.addFinalTask {
+            player.attack(target)
+            context.assertEquals(target.health, currentHealth, "Target health changed")
+            context.assertEquals(target.primeAdversary, player, "Target prime adversary changed")
         }
     }
 }
