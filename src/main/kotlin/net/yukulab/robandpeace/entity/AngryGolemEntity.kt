02879751e60c892dc14b0572fe63c8681a4d6b0d
turtton <top.gear7509@turtton.net>
2024-11-24 19:57:20 +0900

    fix: Store targeted player to keep tracking after lost

--- src/main/kotlin/net/yukulab/robandpeace/entity/AngryGolemEntity.kt
+++ src/main/kotlin/net/yukulab/robandpeace/entity/AngryGolemEntity.kt
@@ -2,18 +2,25 @@ package net.yukulab.robandpeace.entity
 
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.passive.IronGolemEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.world.World
 import net.yukulab.robandpeace.config.RapConfigs
 
 class AngryGolemEntity(type: EntityType<AngryGolemEntity>, world: World) : IronGolemEntity(type, world) {
+    private var lastTargetPlayer: PlayerEntity? = null
+
     init {
         chooseRandomAngerTime()
     }
 
     override fun tick() {
         super.tick()
+        if (target == null && lastTargetPlayer != null && canTarget(lastTargetPlayer)) {
+            target = lastTargetPlayer
+        }
         if (!hasAngerTime()) {
             discard()
         }
@@ -29,5 +36,12 @@ class AngryGolemEntity(type: EntityType<AngryGolemEntity>, world: World) : IronG
 
     override fun dropXp(attacker: Entity?) {
         // Do not drop XP
+    }
+
+    override fun setTarget(target: LivingEntity?) {
+        super.setTarget(target)
+        if (target is PlayerEntity) {
+            lastTargetPlayer = target
+        }
     }
 }
