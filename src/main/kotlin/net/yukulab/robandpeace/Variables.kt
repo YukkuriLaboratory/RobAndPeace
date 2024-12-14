0f57f727f776a8de4d35c6a8928dc440b25562ea
sysnote8main <sysnote8main@gmail.com>
2024-12-14 09:29:18 +0900

    [Add] methods for identifier creation

--- src/main/kotlin/net/yukulab/robandpeace/Variables.kt
+++ src/main/kotlin/net/yukulab/robandpeace/Variables.kt
@@ -1,5 +1,11 @@
 package net.yukulab.robandpeace
 
+import net.minecraft.util.Identifier
+
 const val MOD_ID = "robandpeace"
 
 const val NAMESPACE = "Rob&Peace"
+
+fun id(path: String): Identifier = Identifier.of(MOD_ID, path)
+
+fun getTextureId(path: String): Identifier = id("textures/$path")
