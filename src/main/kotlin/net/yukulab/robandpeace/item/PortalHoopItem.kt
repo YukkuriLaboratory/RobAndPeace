74c298b8f5d8c3b7e9985ae237e51b819a62d00c
sysnote8main <sysnote8main@gmail.com>
2024-12-14 12:51:03 +0900

    temp commit

--- src/main/kotlin/net/yukulab/robandpeace/item/PortalHoopItem.kt
+++ src/main/kotlin/net/yukulab/robandpeace/item/PortalHoopItem.kt
@@ -1,5 +1,6 @@
 package net.yukulab.robandpeace.item
 
+import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
 import net.minecraft.item.Item
 import net.minecraft.item.ItemUsageContext
@@ -46,53 +47,115 @@ class PortalHoopItem : Item(Settings()) {
         // Logic
         logger.info("Portal Hoop was used! side: {}, sideOffset -> (x:{}, y:{}, z:{})", side, side.offsetX, side.offsetY, side.offsetZ)
 
-        // TODO Fix need on implement gravity item
-        // n:z-,s:z+ / w:x-,e:x+
+        // === Portal origin block positions ===
         val portalBasePos = context.blockPos.add(side.offsetX, side.offsetY, side.offsetZ)
         val portalExtendPos: BlockPos = getExtendPos(side, context.horizontalPlayerFacing, portalBasePos)
 
-        placeDebugBlock(context.world, portalBasePos, portalExtendPos)
-
-        logger.info("BasePos: {}, ExtendPos: {}", portalBasePos, portalExtendPos)
-
-        // Yの+1は、プレイヤーの高さの2ブロックで探査する都合上
-        val cacheEndPos = context.blockPos.add(-side.offsetX * maxRange, -side.offsetY * maxRange + 1, -side.offsetZ * maxRange)
-        val worldCache = ChunkCache(
-            context.world,
-            context.blockPos,
-            cacheEndPos,
-        )
-
-        // Search air
-        val searchPos = context.blockPos.mutableCopy()
-        var flag = false
-        for (i in 1..maxRange) {
-            searchPos.move(-side.offsetX, -side.offsetY, -side.offsetZ)
-            val state = worldCache.getBlockState(searchPos)
-            logger.info("Pos: {}, IsAir: {}", searchPos, state.isAir)
-            if (state.isAir) {
-                logger.info("Air found!")
-                context.player!!.sendMessage(Text.of("Found air!"))
-                flag = true
-                break
-            }
-        }
-
-        if (!flag) {
-            logger.info("Air not found in range. rangeSize: $maxRange")
-            context.player!!.sendMessage(Text.of("Air not found in range."))
-
+        if (checkAirAreaFromWorld(context.world, portalBasePos, portalExtendPos)) {
+            placeDebugBlock(context.world, portalBasePos, portalExtendPos)
+        } else {
+            context.player!!.sendMessage(Text.of("Can't place block!!!! (debug message)"))
             return ActionResult.FAIL
         }
 
-        placeDebugBlock(context.world, searchPos, searchPos.add(0, 1, 0))
+        // logger.info("BasePos: {}, ExtendPos: {}", portalBasePos, portalExtendPos)
+
+        // === Search Air area ===
+        val destinationPos: BlockPos = searchAirArea(
+            context.world,
+            context.blockPos,
+            Direction.UP, // because only support y+1 block as extension block. TODO: support gravity-api
+            context.horizontalPlayerFacing,
+            maxRange,
+        ).getOrElse {
+            logger.error("Failed to search air area")
+            context.player!!.sendMessage(Text.of("Air area not found."))
+            return ActionResult.FAIL
+        }
+
+        placeDebugBlock(context.world, destinationPos, destinationPos.add(0, 1, 0))
 
         // === Let's place portal ===
         val destDimKey: RegistryKey<World> = (context.player ?: error("Failed to get player dimension registrykey")).world.registryKey
 
-        createPortal(context.world, portalBasePos.add(0, 1, 0), destDimKey, searchPos.add(0, 1, 0))
+        createPortal(context.world, portalBasePos.add(0, 1, 0), destDimKey, destinationPos.add(0, 1, 0))
 
         return ActionResult.SUCCESS
+    }
+
+    /**
+     * Search air area to place the portal
+     *
+     * @param targetWorld world of search target
+     * @param interactBlockPos blockPos of player interacted block
+     * @param extensionDirection direction of which to extend the portal
+     * @param playerFacing direction of player facing
+     * @param maximumSearchRange maximum range of search
+     * @return result of search as Result<BlockPos>
+     */
+    // TODO: support gravity api
+    private fun searchAirArea(
+        targetWorld: World,
+        interactBlockPos: BlockPos,
+        extensionDirection: Direction,
+        playerFacing: Direction,
+        maximumSearchRange: Int,
+    ): Result<BlockPos> {
+        // 1. Cache
+        val cacheStartPos = interactBlockPos
+        val cacheEndPos = interactBlockPos.add( // Long side
+            playerFacing.offsetX * maximumSearchRange,
+            playerFacing.offsetY * maximumSearchRange,
+            playerFacing.offsetZ * maximumSearchRange,
+        ).offset(extensionDirection)
+        val chunkCache = ChunkCache(
+            targetWorld,
+            cacheStartPos,
+            cacheEndPos,
+        )
+
+        // 2. Search
+        val searchPos = interactBlockPos.mutableCopy()
+        for (i in 1..maxRange) {
+            searchPos.move(playerFacing)
+            if (checkAirArea(chunkCache, searchPos, extensionDirection)) {
+                logger.info("Air found! Pos: {}", searchPos)
+                return Result.success(searchPos)
+            }
+        }
+
+        // 3. Return
+        logger.info("Air not found.")
+        return Result.failure(RuntimeException("Air was not found in this area."))
+    }
+
+    private fun checkAirArea(chunkCache: ChunkCache, basePos: BlockPos, extensionDirection: Direction): Boolean {
+        val extendPos = basePos.offset(extensionDirection)
+
+        // Check range is cached
+        if (chunkCache.isOutOfHeightLimit(basePos) || chunkCache.isOutOfHeightLimit(extendPos)) {
+            error("Pos is out of bounds for chunk cache")
+        }
+
+        val baseBlockState: BlockState = chunkCache.getBlockState(basePos)
+        val extBlockState: BlockState = chunkCache.getBlockState(extendPos)
+        logger.info("loop baseBlockId: ${baseBlockState.block.name}, coord: $basePos")
+        logger.info("loop extBlockId: ${extBlockState.block.name}, coord: $extendPos")
+
+        // return both blocks are air
+        return baseBlockState.isAir && extBlockState.isAir
+    }
+
+    private fun checkAirAreaFromWorld(world: World, basePos: BlockPos, extensionBlockPos: BlockPos): Boolean {
+        logger.info("Base:{}, Extend:{}", basePos, extensionBlockPos)
+
+        // Check range is cached
+        if (world.isOutOfHeightLimit(basePos) || world.isOutOfHeightLimit(extensionBlockPos)) {
+            error("Pos is out of bounds for chunk cache")
+        }
+
+        // return both blocks are air
+        return world.getBlockState(basePos).isAir && world.getBlockState(extensionBlockPos).isAir
     }
 
     private fun createPortal(
