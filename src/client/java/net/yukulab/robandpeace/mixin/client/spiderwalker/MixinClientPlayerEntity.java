9416e8d42b433d690fece89bda49c684ce749635
sysnote8main <sysnote8main@gmail.com>
2024-12-24 21:24:00 +0900

    feat: send payload from client

--- src/client/java/net/yukulab/robandpeace/mixin/client/spiderwalker/MixinClientPlayerEntity.java
+++ src/client/java/net/yukulab/robandpeace/mixin/client/spiderwalker/MixinClientPlayerEntity.java
@@ -2,31 +2,69 @@ package net.yukulab.robandpeace.mixin.client.spiderwalker;
 
 import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import com.mojang.authlib.GameProfile;
+import com.mojang.logging.LogUtils;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.input.Input;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.yukulab.robandpeace.network.payload.PlayerMovementPayload;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ClientPlayerEntity.class, priority = 999)
 public abstract class MixinClientPlayerEntity extends PlayerEntity {
-
     public MixinClientPlayerEntity(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
     }
+
+    private static Logger logger = LogUtils.getLogger();
+
     @Shadow
     protected abstract boolean canSprint();
-    @Shadow public Input input;
+
+    @Shadow
+    public Input input;
+
+    @Unique
+    private PlayerMovementPayload sentPayload = null;
+
+    @Inject(
+            method = "tick",
+            at = @At(value = "HEAD")
+    )
+    private void onTick(CallbackInfo ci) {
+        // First synchronization
+        if (sentPayload == null) {
+            sentPayload = new PlayerMovementPayload(input.hasForwardMovement(), input.movementForward, input.jumping);
+            logger.info("First player movement syncing...");
+            ClientPlayNetworking.send(sentPayload);
+            logger.info("First player movement sync was completed.");
+            return;
+        }
+
+        boolean isDirty = false;
+        if (sentPayload.getHasForwardMovement() != input.hasForwardMovement()) isDirty = true;
+        if (sentPayload.getMovementForward() != input.movementForward) isDirty = true;
+        if (sentPayload.isJumping() != input.jumping) isDirty = true;
+        if(isDirty) {
+            sentPayload = new PlayerMovementPayload(input.hasForwardMovement(), input.movementForward, input.jumping);
+            ClientPlayNetworking.send(sentPayload);
+        }
+    }
 
     /**
      * Removes sprinting loss on collision, strafing, and more.
      */
     @ModifyReceiver(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;setSprinting(Z)V"))
     private ClientPlayerEntity removeSprintingLogic(ClientPlayerEntity clientPlayerEntity, boolean sprinting) {
-        if (sprinting || !this.canSprint() || !(this.input.movementForward > 1.0E-5F)){
+        if (sprinting || !this.canSprint() || !(this.input.movementForward > 1.0E-5F)) {
             this.setSprinting(sprinting);
         }
         return clientPlayerEntity;
