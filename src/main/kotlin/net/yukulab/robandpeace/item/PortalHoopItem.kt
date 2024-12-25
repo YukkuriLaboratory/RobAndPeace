d0e3b8071f5c7be16f777a6ef0df7a3818f86b4a
sysnote8main <sysnote8main@gmail.com>
2024-12-25 12:25:32 +0900

    add: last used timestamp for stop use function calling twice

--- src/main/kotlin/net/yukulab/robandpeace/item/PortalHoopItem.kt
+++ src/main/kotlin/net/yukulab/robandpeace/item/PortalHoopItem.kt
@@ -10,6 +10,7 @@ import net.minecraft.server.world.ServerWorld
 import net.minecraft.text.Text
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
+import net.minecraft.util.Identifier
 import net.minecraft.util.TypedActionResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
@@ -17,11 +18,13 @@ import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 import net.minecraft.world.chunk.ChunkCache
 import net.yukulab.robandpeace.DelegatedLogger
+import net.yukulab.robandpeace.MOD_ID
 import net.yukulab.robandpeace.entity.RapEntityType
 import net.yukulab.robandpeace.entity.ThroughHoopPortal
 import net.yukulab.robandpeace.item.component.RapComponents
 import qouteall.imm_ptl.core.McHelper
 import qouteall.imm_ptl.core.api.PortalAPI
+import qouteall.imm_ptl.core.portal.Portal
 import qouteall.imm_ptl.core.portal.PortalManipulation
 import qouteall.imm_ptl.core.portal.global_portals.GlobalPortalStorage
 import qouteall.q_misc_util.my_util.AARotation
@@ -32,6 +35,9 @@ class PortalHoopItem : Item(Settings()) {
     companion object {
         private val logger by DelegatedLogger()
         private const val MAX_RANGE = 6
+
+        const val SUFFIX_REMOVE_MODE: String = "_remove"
+        val KEY_REMOVE_MODE: Identifier = Identifier.of(MOD_ID, "remove")
 
         private fun getExtendPos(side: Direction, playerFacing: Direction, basePos: BlockPos): BlockPos = when (side) {
             Direction.UP -> basePos.mutableCopy().move(
@@ -50,6 +56,12 @@ class PortalHoopItem : Item(Settings()) {
         }
     }
 
+    override fun getTranslationKey(stack: ItemStack?): String = if (stack?.get(RapComponents.PORTAL_HOOP_IS_REMOVE_MODE) == true) {
+        "${translationKey}${SUFFIX_REMOVE_MODE}"
+    } else {
+        super.getTranslationKey(stack)
+    }
+
     override fun useOnBlock(context: ItemUsageContext): ActionResult {
         // If client side
         if (context.world.isClient) return super.useOnBlock(context)
@@ -59,6 +71,8 @@ class PortalHoopItem : Item(Settings()) {
         // this stack is not in remove mode = place mode
         if (heldStack.get(RapComponents.PORTAL_HOOP_IS_REMOVE_MODE) != true) {
             // On place or First use
+            logger.debug("Portal placing...")
+            heldStack.set(RapComponents.PORTAL_HOOP_LAST_USED, context.world.time)
             heldStack.set(RapComponents.PORTAL_HOOP_IS_REMOVE_MODE, true)
             return onPlacePortal(context, heldStack)
         }
@@ -67,11 +81,19 @@ class PortalHoopItem : Item(Settings()) {
     }
 
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+        if (world.isClient) return super.use(world, user, hand)
+
         val heldStack: ItemStack = user.getStackInHand(hand)
 
         // If in remove mode, remove portals.
         if (heldStack.get(RapComponents.PORTAL_HOOP_IS_REMOVE_MODE) == true) {
             // On remove
+            val lastUsedTime: Long = heldStack.get(RapComponents.PORTAL_HOOP_LAST_USED) ?: -1
+            if (world.time - lastUsedTime < 2) {
+                return TypedActionResult.fail(heldStack)
+            }
+
+            logger.debug("Portal removing...")
             heldStack.set(RapComponents.PORTAL_HOOP_IS_REMOVE_MODE, false)
             return onRemovePortal(world, user, heldStack)
         }
@@ -134,7 +156,7 @@ class PortalHoopItem : Item(Settings()) {
 
     private fun onRemovePortal(world: World, user: PlayerEntity, heldStack: ItemStack): TypedActionResult<ItemStack> {
         // Get all portals
-        val portals = GlobalPortalStorage.getGlobalPortals(world)
+        val portals: MutableList<Portal> = GlobalPortalStorage.getGlobalPortals(world).toMutableList()
         portals.addAll( // Add nearby portals
             McHelper.getEntitiesNearby(
                 user,
@@ -154,6 +176,11 @@ class PortalHoopItem : Item(Settings()) {
         // Search origin & destination portals
         val originPortal = portals.find { p -> p.id == originId }
         val destPortal = portals.find { p -> p.id == destId }
+
+        if (originPortal == null || destPortal == null) {
+            logger.warn("Failed to find portals")
+            return TypedActionResult.fail(heldStack)
+        }
 
         // Remove both portals
         val serverWorld = world as ServerWorld
--- src/main/kotlin/net/yukulab/robandpeace/item/component/RapComponents.kt
+++ src/main/kotlin/net/yukulab/robandpeace/item/component/RapComponents.kt
@@ -14,6 +14,7 @@ object RapComponents {
 
     // Portal hoop components
     val PORTAL_HOOP_IS_REMOVE_MODE: ComponentType<Boolean> = register("portal_hoop_is_remove_mode", ComponentType.builder<Boolean>().codec(Codec.BOOL))
+    val PORTAL_HOOP_LAST_USED: ComponentType<Long> = register("portal_hoop_last_used", ComponentType.builder<Long>().codec(Codec.LONG))
     val PORTAL_ID_ORIGIN: ComponentType<Int> = register("portal_id_origin", ComponentType.builder<Int>().codec(Codec.INT))
     val PORTAL_ID_DESTINATION: ComponentType<Int> = register("portal_id_destination", ComponentType.builder<Int>().codec(Codec.INT))
 
