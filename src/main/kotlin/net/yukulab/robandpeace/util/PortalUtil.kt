37c867dfab13a2d6feb488fd0ca7194b54a7efef
sysnote8main <sysnote8main@gmail.com>
2024-12-31 18:11:46 +0900

    feat: add created portals to GlobalPortalStorage

--- src/main/kotlin/net/yukulab/robandpeace/util/PortalUtil.kt
+++ src/main/kotlin/net/yukulab/robandpeace/util/PortalUtil.kt
@@ -1,6 +1,7 @@
 package net.yukulab.robandpeace.util
 
 import net.minecraft.registry.RegistryKey
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Vec3d
@@ -11,6 +12,7 @@ import net.yukulab.robandpeace.entity.RapEntityType
 import net.yukulab.robandpeace.entity.ThroughHoopPortal
 import qouteall.imm_ptl.core.api.PortalAPI
 import qouteall.imm_ptl.core.portal.PortalManipulation
+import qouteall.imm_ptl.core.portal.global_portals.GlobalPortalStorage
 import qouteall.q_misc_util.my_util.AARotation
 
 data class PortalData(val origin: ThroughHoopPortal, val destination: ThroughHoopPortal)
@@ -19,7 +21,7 @@ data class OffsetData(val baseOffset: Double, val extOffset: Double)
 
 object PortalUtil {
     fun createPortal(
-        world: World,
+        world: ServerWorld,
         originPos: Vec3d,
         wallFacing: Direction,
         destinationDim: RegistryKey<World>,
@@ -61,6 +63,11 @@ object PortalUtil {
         // Spawn portals
         world.spawnEntity(portal) // Origin
         world.spawnEntity(destinationPortal) // Destination
+
+        GlobalPortalStorage.get(world).apply {
+            addPortal(portal)
+            addPortal(destinationPortal)
+        }
 
         return PortalData(portal, destinationPortal)
     }
