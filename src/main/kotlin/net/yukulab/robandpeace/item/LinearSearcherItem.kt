9aeec500a686d826ecefb68f6240699802f73d92
sysnote8main <sysnote8main@gmail.com>
2024-12-31 15:07:35 +0900

    feat: calculate correct vec3d

--- src/main/kotlin/net/yukulab/robandpeace/item/LinearSearcherItem.kt
+++ src/main/kotlin/net/yukulab/robandpeace/item/LinearSearcherItem.kt
@@ -9,6 +9,7 @@ import net.minecraft.text.Text
 import net.minecraft.util.ActionResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3d
+import net.minecraft.world.chunk.ChunkCache
 import net.yukulab.robandpeace.DelegatedLogger
 
 class LinearSearcherItem : Item(Settings().maxCount(1)) {
@@ -21,28 +22,41 @@ class LinearSearcherItem : Item(Settings().maxCount(1)) {
         val portalOriginPos = context.hitPos.floor()
         val exploreDirection = context.side.opposite
 
+        // Cache world
+        val worldCache = ChunkCache(
+            context.world,
+            portalOriginPos.toBlockPos(),
+            portalOriginPos.toBlockPos().offset(exploreDirection, SEARCH_MAX).up(),
+        )
+
         val currentPos = portalOriginPos.toBlockPos().mutableCopy()
-        for (i in 0..10) {
+        for (i in 0..SEARCH_MAX) {
             logger.info("Place loop in $i")
 
             // Move to exploreDirection
             currentPos.move(exploreDirection)
 
             // Get state
-            val currentState = context.world.getBlockState(currentPos)
+            val currentState = worldCache.getBlockState(currentPos)
             val calculatedIsAir = currentState.block == Blocks.AIR
             logger.info("Pos: $currentPos, BlockId: ${currentState.block.name}, IsAir:${currentState.isAir}, =AIR:$calculatedIsAir")
 
+            // If under block is air
             if (calculatedIsAir) {
                 logger.info("Checking upper block....")
-                val extCurrentState = context.world.getBlockState(currentPos.up())
+                val extCurrentState = worldCache.getBlockState(currentPos.up())
                 val extCalculatedIsAir = extCurrentState.block == Blocks.AIR
+
                 if (extCalculatedIsAir) {
+                    // If upper block is air too
                     logger.info("This block is equal as air block!")
                     logger.info("Found it! pos: $currentPos")
                     context.player?.sendMessage(Text.of("Found it! pos: $currentPos"))
+                    val actualDestVec = currentPos.toCenterPos().offset(exploreDirection, -0.4)
+                    logger.info("Actual destination -> $actualDestVec")
                     return ActionResult.SUCCESS
                 } else {
+                    // If upper block wasn't air
                     logger.info("Upper block is not air. continue...")
                     continue
                 }
@@ -61,6 +75,7 @@ class LinearSearcherItem : Item(Settings().maxCount(1)) {
     private fun Vec3d.toBlockPos() = BlockPos(x.toCeilInt(), y.toCeilInt(), z.toCeilInt())
 
     companion object {
+        private val SEARCH_MAX = 10
         private val logger by DelegatedLogger()
     }
 }
