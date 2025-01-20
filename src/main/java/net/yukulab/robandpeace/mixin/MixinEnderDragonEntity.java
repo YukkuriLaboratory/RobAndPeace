0facf2f4eff9484c0478bdf0a0df6f457507ffe4
turtton <top.gear7509@turtton.net>
2025-01-20 20:00:44 +0900

    feat: Change treasure drop position
    ポータルにボッシュートしないように

--- src/main/java/net/yukulab/robandpeace/mixin/MixinEnderDragonEntity.java
+++ src/main/java/net/yukulab/robandpeace/mixin/MixinEnderDragonEntity.java
@@ -1,21 +1,24 @@
 package net.yukulab.robandpeace.mixin;
 
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.gen.feature.EndPortalFeature;
 import net.yukulab.robandpeace.item.RapItems;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EnderDragonEntity.class)
-public class MixinEnderDragonEntity {
+public abstract class MixinEnderDragonEntity {
     @Inject(
             method = "kill",
             at = @At("HEAD")
     )
     private void dropHiddenTreasure(CallbackInfo ci) {
-        var dragon = (EnderDragonEntity) (Object) this;
-        dragon.dropItem(RapItems.INSTANCE.getHIDDEN_TREASURE());
+        robandpeace$dropHiddenTreasureItem();
     }
 
     @Inject(
@@ -26,7 +29,16 @@ public class MixinEnderDragonEntity {
             )
     )
     private void dropHiddenTreasure2(CallbackInfo ci) {
+        robandpeace$dropHiddenTreasureItem();
+    }
+
+    @Unique
+    private void robandpeace$dropHiddenTreasureItem() {
         var dragon = (EnderDragonEntity) (Object) this;
-        dragon.dropItem(RapItems.INSTANCE.getHIDDEN_TREASURE());
+        var itemStack = RapItems.INSTANCE.getHIDDEN_TREASURE().getDefaultStack();
+        var origin = dragon.getWorld().getTopPosition(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, EndPortalFeature.offsetOrigin(dragon.getFightOrigin())).up(3);
+        var itemEntity = new ItemEntity(dragon.getWorld(), origin.getX() + 3, origin.getY(), origin.getZ() + 3, itemStack);
+        itemEntity.setToDefaultPickupDelay();
+        dragon.getWorld().spawnEntity(itemEntity);
     }
 }
